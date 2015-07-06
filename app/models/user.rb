231f32a2ac30030798598bb31b1a7cd591cbdb62
Krzysztof Kołek <krzysiek.koek6@gmail.com>
2015-07-06 22:20:19 +0200

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :full_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  validates :display_name, length: { in: 2..32 }, allow_blank: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(full_name: 'Martin Luther', email: 'martin@luther.com') }
+
+  it 'is valid' do
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a full_name' do
+    user.full_name = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without an email address' do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid with the same address' do
+    user.save
+    user_copy = User.create(email: user.email, full_name: user.full_name)
+    expect(user_copy.valid?).to eq(false)
+  end
+
+  it 'is not valid with the same address uppercase' do
+    user.save
+    user_copy = User.create(email: user.email.upcase, full_name: user.full_name)
+    expect(user_copy.valid?).to eq(false)
+  end
+
+  it 'is not valid with incorrect email address' do
+    invalid_addresses = %w[foo@example,com user_at_bar.org user.name@pass.
+                        word@i_am_.com man@ea+ter.de]
+    invalid_addresses.each do |ia|
+      user.email = ia
+      expect(user).to_not be_valid
+    end
+  end
+
+  it 'is valid with correct email address' do
+    valid_addresses = %w[user@name.com R_TDD-DHH@rails.ruby.org admin@example.com
+			first.last@user.name laura+joe@stephen.cm]
+    valid_addresses.each do |va|
+      user.email = va
+      expect(user).to be_valid
+    end
+  end
+
+  it 'display name must be at least 2 characters' do
+    user.display_name = 'a'
+    expect(user).to_not be_valid
+  end
+
+  it 'display name must be at most 32 characters' do
+    user.display_name = 'a' * 33
+    expect(user).to_not be_valid
+  end
 end
