f5817dd32b3adcc84f1e24360326808107ccb2a6
Krzysiek Kołek <krzysiek.koek6@gmail.com>
2015-08-05 21:08:08 +0200

    Add #user_logged_in? helper test

--- spec/helpers/user_helper_spec.rb
+++ spec/helpers/user_helper_spec.rb
@@ -14,4 +14,17 @@ describe UserHelper do
       expect(helper.current_user).to eq(user)
     end
   end
+
+  describe '#user_logged_in' do
+    it 'return true for logged in user' do
+      user = create(:user)
+      session[:user_id] = user.id
+      expect(helper.user_logged_in?).to be(true)
+    end
+
+    it 'return false for not logged user' do
+      session[:user_id] = nil
+      expect(helper.user_logged_in?).to be(false)
+    end
+  end
 end
