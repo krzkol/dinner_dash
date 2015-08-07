806204570c66321455fdbeada3d3db349cdb91c4
Krzysiek Kołek <krzysiek.koek6@gmail.com>
2015-08-07 08:59:22 +0200

    Fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,66 +1,128 @@
-c1 = Category.find_or_create_by(name: 'Drinks')
-c2 = Category.find_or_create_by(name: 'Vegetables')
-c3 = Category.find_or_create_by(name: 'Meat')
-c4 = Category.find_or_create_by(name: 'Fast food')
-c5 = Category.find_or_create_by(name: 'Deserts')
-c6 = Category.find_or_create_by(name: 'No meat')
-c7 = Category.find_or_create_by(name: 'Fishes')
+c1 = Category.find_or_create_by!(name: 'Drinks')
+c2 = Category.find_or_create_by!(name: 'Vegetables')
+c3 = Category.find_or_create_by!(name: 'Meat')
+c4 = Category.find_or_create_by!(name: 'Fast food')
+c5 = Category.find_or_create_by!(name: 'Deserts')
+c6 = Category.find_or_create_by!(name: 'No meat')
+c7 = Category.find_or_create_by!(name: 'Fishes')
 
 
-item = Item.find_or_create_by(title: 'Chips', description: 'Tasty chips with ketchup', price: 3.00)
-item.categories = [c4]
-item = Item.find_or_create_by(title: 'Baked chicken', description: 'Chicken baked in oven', price: 12.00)
-item.categories = [c3]
-item = Item.find_or_create_by(title: 'Tomato salad', description: 'Salad from tomatoes and onions', price: 5.70)
-item.categories = [c2]
-item = Item.find_or_create_by(title: 'Grilled sausage', description: 'Sausage from the grill', price: 4.75)
-item.categories = [c3, c4]
-item = Item.find_or_create_by(title: 'Pizza with chicken', description: 'Delicious pizza with parts of chicken', price: 25.00)
-item.categories = [c3, c4]
-item = Item.find_or_create_by(title: 'Tea', description: 'Black tea', price: 3.50)
-item.categories = [c1]
-item = Item.find_or_create_by(title: 'Coffee', description: 'Instant coffee', price: 4.00)
-item.categories = [c1]
-item = Item.find_or_create_by(title: 'Mineral water', description: 'Fresh mineral water', price: 2.50)
-item.categories = [c1]
-item = Item.find_or_create_by(title: 'Hot dog', description: 'Tasty hot dog with ketchup', price: 4.20)
-item.categories = [c3, c4]
-item = Item.find_or_create_by(title: 'Hamburger', description: 'Delicious hamburger with extra meat', price: 5.60)
-item.categories = [c3, c4]
-item = Item.find_or_create_by(title: 'Bigos', description: 'Cabbage with meat', price: 7.80)
-item.categories = [c2, c3]
-item = Item.find_or_create_by(title: 'Pierogi ruskie', description: 'Potatoes and curd inside pie', price: 7.00)
-item.categories = [c6]
-item = Item.find_or_create_by(title: 'Apple pie', description: 'Apples with pie', price: 3.00)
-item.categories = [c5]
-item = Item.find_or_create_by(title: 'Ice creams', description: 'Cold chocolate ice creams', price: 1.90)
-item.categories = [c5]
-item = Item.find_or_create_by(title: 'Beer', description: 'Cold beer', price: 2.80)
-item.categories = [c1]
-item = Item.find_or_create_by(title: 'Vodka', description: 'Burn as hell', price: 18.00)
-item.categories = [c1]
-item = Item.find_or_create_by(title: 'Fried fish', description: 'Fish fried on a pan', price: 10.20)
-item.categories = [c7]
-item = Item.find_or_create_by(title: 'New potatoes', description: 'New and tasty potatoes', price: 5.40)
-item.categories = [c2]
-item = Item.find_or_create_by(title: 'Groats', description: 'Barley groats', price: 8.15)
-item.categories = [c6]
-item = Item.find_or_create_by(title: 'Rice', description: 'White rice', price: 6.30)
-item.categories = [c6]
+Item.find_or_create_by!(title: 'Chips') do |item|
+  item.description = 'Tasty chips with ketchup'
+  item.price = 3.00
+  item.categories << c4
+end
+Item.find_or_create_by!(title: 'Baked chicken') do |item|
+  item.description = 'Chicken baked in oven'
+  item.price = 12.00
+  item.categories << c3
+end
+Item.find_or_create_by!(title: 'Tomato salad') do |item|
+  item.description = 'Salad from tomatoes and onions'
+  item.price = 5.70
+  item.categories << c2
+end
+Item.find_or_create_by!(title: 'Grilled sausage') do |item|
+  item.description = 'Sausage from the grill'
+  item.price = 4.75
+  item.categories << c3 << c4
+end
+Item.find_or_create_by!(title: 'Pizza with chicken') do |item|
+  item.description = 'Delicious pizza with parts of chicken'
+  item.price = 25.00
+  item.categories << c3 << c4
+end
+Item.find_or_create_by!(title: 'Tea') do |item|
+  item.description = 'Black tea'
+  item.price = 3.50
+  item.categories << c1
+end
+Item.find_or_create_by!(title: 'Coffee') do |item|
+  item.description = 'Instant coffee'
+  item.price = 4.00
+  item.categories << c1
+end
+Item.find_or_create_by!(title: 'Mineral water') do |item|
+  item.description = 'Fresh mineral water'
+  item.price = 2.50
+  item.categories << c1
+end
+Item.find_or_create_by!(title: 'Hot dog') do |item|
+  item.description = 'Tasty hot dog with ketchup'
+  item.price = 4.20
+  item.categories << c3 << c4
+end
+Item.find_or_create_by!(title: 'Hamburger') do |item|
+  item.description = 'Delicious hamburger with extra meat'
+  item.price = 5.60
+  item.categories << c3 << c4
+end
+Item.find_or_create_by!(title: 'Bigos') do |item|
+  item.description = 'Cabbage with meat'
+  item.price = 7.80
+  item.categories << c2 << c3
+end
+Item.find_or_create_by!(title: 'Pierogi ruskie') do |item|
+  item.description = 'Potatoes and curd inside pie'
+  item.price = 7.00
+  item.categories << c6
+end
+Item.find_or_create_by!(title: 'Apple pie') do |item|
+  item.description = 'Apples with pie'
+  item.price = 3.00
+  item.categories << c5
+end
+Item.find_or_create_by!(title: 'Ice creams') do |item|
+  item.description = 'Cold chocolate ice creams'
+  item.price = 1.90
+  item.categories << c5
+end
+Item.find_or_create_by!(title: 'Beer') do |item|
+  item.description = 'Cold beer'
+  item.price = 2.80
+  item.categories << c1
+end
+Item.find_or_create_by!(title: 'Vodka') do |item|
+  item.description = 'Burn as hell'
+  item.price = 18.00
+  item.categories << c1
+end
+Item.find_or_create_by!(title: 'Fried fish') do |item|
+  item.description = 'Fish fried on a pan'
+  item.price = 10.20
+  item.categories << c7
+end
+Item.find_or_create_by!(title: 'New potatoes') do |item|
+  item.description = 'New and tasty potatoes'
+  item.price = 5.40
+  item.categories << c2
+end
+Item.find_or_create_by!(title: 'Groats') do |item|
+  item.description = 'Barley groats'
+  item.price = 8.15
+  item.categories << c6
+end
+Item.find_or_create_by!(title: 'Rice') do |item|
+  item.description = 'White rice'
+  item.price = 6.30
+  item.categories << c6
+end
 
-User.where(email: 'demo+rachel@jumpstartlab.com').first_or_create(first_name: 'Rachel', last_name: 'Werbelow', password: 'password')
-User.where(email: 'demo+jeff@jumpstartlab.com').first_or_create(first_name: 'Jeff', last_name: 'Casimir', password: 'password', display_name: 'j3')
-User.where(email: 'demo+jorge@jumpstartlab.com').first_or_create(first_name: 'Jorge', last_name: 'Tellez', password: 'password', display_name: 'novohispano')
-User.where(email: 'demo+josh@jumpstartlab.com').first_or_create(first_name: 'Josh', last_name: 'Cheek', password: 'password', display_name: 'josh', admin: true)
+User.where(email: 'demo+rachel@jumpstartlab.com').first_or_create!(first_name: 'Rachel', last_name: 'Werbelow', password: 'password')
+User.where(email: 'demo+jeff@jumpstartlab.com').first_or_create!(first_name: 'Jeff', last_name: 'Casimir', password: 'password', display_name: 'j3')
+User.where(email: 'demo+jorge@jumpstartlab.com').first_or_create!(first_name: 'Jorge', last_name: 'Tellez', password: 'password', display_name: 'novohispano')
+User.where(email: 'demo+josh@jumpstartlab.com').first_or_create!(first_name: 'Josh', last_name: 'Cheek', password: 'password', display_name: 'josh', admin: true)
 
 statuses = %w[ordered paid completed cancelled]
 10.times do |o|
-  order = Order.new(status: statuses[o % 4])
-  (rand(3) + 1).times do |oi|
+  cart = Cart.new
+  (rand(6) + 1).times do |oi|
     id = rand(20) + 1
-    qty = rand(20) + 1
-    order.order_items.build(item_id: id, quantity: qty, price: Item.find(id).price, item_group: order)
-    order.user_id = rand(3) + 1
+    cart.add_item(id)
   end
+  order = Order.new
+  order.status = statuses[o%4]
+  order.user_id = rand(3) + 1
+  order.copy_items_from_cart(cart)
   order.save!
 end
